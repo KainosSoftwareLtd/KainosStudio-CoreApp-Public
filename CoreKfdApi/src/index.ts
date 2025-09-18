6d315964f375546b1186f7f3364d6062de3c6a7a
Jarosław Rejterada <jaroslaw.rejterada@kainos.com>
2025-09-18 13:53:36 +0200

    KFP-796 increased limit to 6MB (max for awsx lambdas)

--- CoreKfdApi/src/index.ts
+++ CoreKfdApi/src/index.ts
@@ -16,7 +16,7 @@ export function createApp(): express.Express {
   const app = express();
   app.use(securityHeaders);
   app.use(nocache());
-  app.use(express.json());
+  app.use(express.json({ limit: '6MB' }));
   app.use(express.urlencoded({ extended: true }));
   app.use(authMiddleware);
   app.use(contentTypeValidator);
